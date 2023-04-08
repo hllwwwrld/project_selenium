b0de0feb64660296dc2fb8c59068a33d81e1c670
hllwwwrld <arkoah072@gmail.com>
2023-04-08 15:13:41 +0500

    added case to jump into login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,10 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert 'login' in self.browser.current_url
+        assert 'login' in self.browser.current_url, '"Login" not in current url'
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Registration form is not presented'
--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 class TestMainPage:
@@ -16,7 +17,15 @@ class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, self.link)
         page.open()
-        page.go_to_login_page()
 
+        # 1 вариант переключению на страницу логина - возврат страницы в методе,
+        # где кликаем по ссылке со страницей логина
+        # login_page = page.go_to_login_page()
+        # login_page.should_be_login_page()
+
+        # 2 вариант инициализация новой страницы на основании url, который получен после клика по кнопке авторизации
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
