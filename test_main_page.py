8b9f0b6a1bd684b0b133b37627682243eefbe6ae
hllwwwrld <arkoah072@gmail.com>
2023-04-08 14:27:38 +0500

    added locators for login page

--- test_main_page.py
+++ test_main_page.py
@@ -5,11 +5,18 @@ from .pages.main_page import MainPage
 
 
 class TestMainPage:
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+    link = 'http://selenium1py.pythonanywhere.com/'
+
+    def test_guest_can_see_the_login_link(self, browser):
+        page = MainPage(browser, self.link)
+        page.open()
+        page.should_be_login_link()
 
     def test_guest_can_go_to_login_page(self, browser):
-        page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
+        page = MainPage(browser, self.link)
         page.open()
         page.go_to_login_page()
-        page.should_be_login_link()
+
 
 
