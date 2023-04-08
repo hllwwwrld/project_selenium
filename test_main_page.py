cfcdc0efd925f48ca19d4fa20c56214c162d0570
hllwwwrld <arkoah072@gmail.com>
2023-04-08 13:45:07 +0500

    added pageObject model

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,15 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .pages.main_page import MainPage
 
 
 class TestMainPage:
 
     def test_guest_can_go_to_login_page(self, browser):
-        browser.get("http://selenium1py.pythonanywhere.com/")
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
+        page.open()
+        page.go_to_login_page()
+        page.should_be_login_link()
 
 
