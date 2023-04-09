6b68ffce5e2007ee82af1ffcfe5d6b146146dd9a
hllwwwrld <arkoah072@gmail.com>
2023-04-09 18:24:32 +0500

    fixed mark

--- test_product_page.py
+++ test_product_page.py
@@ -75,7 +75,6 @@ class TestProductPage:
 
     @pytest.mark.need_review
     @pytest.mark.login
-    @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         product_page = ProductPage(browser, link)
